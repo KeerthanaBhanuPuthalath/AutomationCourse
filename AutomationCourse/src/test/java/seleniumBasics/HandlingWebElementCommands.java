49bbedaf2767fbdc216036c7e9b3e0bbb07067cb
KeerthanaBhanuPuthalath <keerthanabputhalath@gmail.com>
2025-06-09 20:51:27 +0530

    Commited

--- AutomationCourse/src/test/java/seleniumBasics/HandlingWebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingWebElementCommands.java
@@ -1,7 +1,15 @@
 package seleniumBasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HandlingWebElementCommands extends Base
 {
@@ -15,6 +23,10 @@ public class HandlingWebElementCommands extends Base
 		WebElement showMessageButton = driver.findElement(By.xpath("//button[@id='button-one']"));
 		System.out.println("Displayed: "+showMessageButton.isDisplayed());
 		System.out.println("Enabled : "+showMessageButton.isEnabled());
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));//explicit wait
+		wait.until(ExpectedConditions.elementToBeClickable(showMessageButton));
+		Wait<WebDriver> fluent =new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(2)).ignoring(NoSuchElementException.class);
+		fluent.until(ExpectedConditions.elementToBeClickable(showMessageButton));
 		showMessageButton.click();
 		WebElement consoleMessage=driver.findElement(By.xpath("//div[@id='message-one']"));
 		System.out.println(consoleMessage.getText());
